74a67882cdd75e9c5f09fb7d0f37dac0aee43e33
designerofthing <designerofthings@gmail.com>
2020-04-23 16:59:34 +0200

    deletes user can view start page

--- spec/features/user_can_view_start_page_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'User can view start page' do
-    before do
-        visit root_path
-    end
-
-    it 'User should see the start page' do
-    expect(page).to have_content 'Craft Academy Mailbox'
-    end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,3 +7,4 @@ RSpec.describe User, type: :model do
         it { is_expected.to have_db_column :encrypted_password }
     end
 end 
+
