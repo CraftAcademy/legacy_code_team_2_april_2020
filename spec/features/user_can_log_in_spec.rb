f7bab9883d7428bdb2f96938de07ce49cab4ae86
japh <jenny.scherr@svenskaspel.se>
2020-04-23 20:22:39 +0200

    put in sad path for user logging in, wrong password

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -13,5 +13,14 @@ feature 'user can log in' do
       click_on "Log in"
       expect(page).to have_content "Signed in successfully."
     end
+
+    describe 'user inputs incorrect credentials' do
+      it 'user input the wrong password' do 
+        fill_in "Email", with: registered_user.email
+        fill_in "Password", with: "hammertime"
+        click_on "Log in"
+        expect(page).to have_content "Invalid Email or password"
+      end
+    end
 end
 
