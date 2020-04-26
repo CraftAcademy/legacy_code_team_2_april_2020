8fe47fee71ccf599aa94513205150d3f48bd82e0
japh <jenny.scherr@svenskaspel.se>
2020-04-26 12:38:33 +0200

    added selenium and chrome driver to gemfile

--- spec/features/user_can_view_deleted_message_spec.rb
+++ spec/features/user_can_view_deleted_message_spec.rb
@@ -1,3 +1,6 @@
+@javascript
+
+
 feature 'user can view deleted message' do
   let(:user) { create(:user, email: "user@gmail.com") }
   let(:sender) { create(:user, name: 'dora', email: "sender@gmail.com") }
@@ -19,56 +22,77 @@ feature 'user can view deleted message' do
       test = find(class: 'media-body')
       expect(test).to have_content 'Lorem ipsum.'
       click_on 'View'
-      accept_alert do
-        click_link ('Move to trash')
+      # accept_alert do
+        click_link('Move to trash')
         # click_button('OK')
-      end
+      # end
     end
 
     it 'message deleted in trash' do
       visit mailbox_trash_path
-      content = find(class: 'media-body')
-      expect(content).to have_content 'Lorem ipsum.'
+      # content = find(class: 'media-body')
+      expect(body).to have_content 'Lorem ipsum.'
     end
   end
 end
 
 
-    # Given(/^I am logged in as "([^"]*)"$/) do |name|
-    #     @user = User.find_by(name: name)
-    #     login_as(@user, scope: :user)
-    # end
 
-    #   And(/^I send a mail to "([^"]*)"$/) do |name|
-    #     @receiver = User.find_by(name: name)
-    #     @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
-    #   end
+# Background
+#   Given following users exists
+#     | name   | email             | password |
+#     | Jenny  | jenny@ranom.com   | password |
+#     | Dora | daniel@random.com | password |
 
-    #   And(/^I should have "([^"]*)" messages$/) do |expected_count|
-    #     count = @receiver.mailbox.inbox.count
-    #     expect(count).to eq expected_count.to_i
-    #     click_on "Logout"
-    #     expect(page).to have_content "Signed out successfully."
-    #   end
+
+# Scenario Deleting a mail
+#   Given I am loggedin as "Dora"
+#   And I send a mail to "Jenny"
+#   And I click on the link "Logout" 
+#   Given I am loggedin as "Jenny"
+#   And I reply a mail to "Dora"
+
+
+#     Given(/^I am logged in as "([^"]*)"$/) do |name|
+#         @user = User.find_by(name: name)
+#         login_as(@user, scope: :user)
+#     end
+
+#     And(/^I send a mail to "([^"]*)"$/) do |name|
+#       @receiver = User.find_by(name: name)
+#       @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
+#     end
+
+#     And('I click on the logout link') do
+#       click_on "Logout"
+#     end
+
+#     Given(/^I am logged in as "([^"]*)"$/) do |name|
+#       visit root_path
+#       login_as(@receiver, scope: :user)
+#     end
+
+#     And(/^I send a mail to "([^"]*)"$/) do |name|
+#       @receiver.reply_to_conversation(@user, 'Lorem ipsum...', 'Subject')
+#     end
+
     
-    #   And(/^I am logged in as "([^"]*)"$/) do |name|
-    #     visit root_path
-    #     login_as(@receiver, scope: :user)
-    #   end
-
-    #   And(/^I send a mail to "([^"]*)"$/) do |name|
-    #     @receiver.reply_to_conversation(@user, 'Lorem ipsum...', 'Subject')
-    #   end
-
+#     Then(/^I should have "([^"]*)" messages$/) do |expected_count|
+#       count = @user.mailbox.inbox.count
+#       expect(count).to eq expected_count.to_i
+#     end
+    
+    # And(/^I should have "([^"]*)" messages$/) do |expected_count|
+    #   count = @receiver.mailbox.inbox.count
+    #   expect(count).to eq expected_count.to_i
+    #   click_on "Logout"
+    #   expect(page).to have_content "Signed out successfully."
     # end
-    #     # Then(/^I should have "([^"]*)" messages$/) do |expected_count|
-    #     #   count = @user.mailbox.inbox.count
-    #     #   expect(count).to eq expected_count.to_i
-    #     # end
-    #     it 'receiver should have 1 new message' do
-    #       count = @user.mailbox.inbox.count
-    #       expect(count).to eq 1
-    #     end
+
+
+
+
+
 
 
 
