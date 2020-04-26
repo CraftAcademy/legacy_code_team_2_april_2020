4d0e5947cfb9506978319df015f5a79bbf74f524
japh <jenny.scherr@svenskaspel.se>
2020-04-26 15:05:39 +0200

    check so that the sender is getting the replied message

--- spec/features/user_can_reply_to_messages_spec.rb
+++ spec/features/user_can_reply_to_messages_spec.rb
@@ -1,35 +1,27 @@
-# frozen_string_literal: true
-
 feature 'User can reply to a message' do
   let(:sender) { create(:user, email: 'sender@mail.com') }
   let(:receiver) { create(:user, name: 'reciever', email: 'sender1@mail.com') }
   before do
     sender.send_message(receiver, 'Lorem ipsum...', 'Subject')
-    login_as(receiver, scope: :user, name: 'reciever', email: 'sender1@mail.com')
-    visit mailbox_inbox_path
-    click_on 'View'
   end
-  it 'user/receiver replies to message' do
-    fill_in 'Reply Message', with: 'Nullam interdum auctor ipsum, cursus luctus ex ullamcorper ac.'
-    click_on 'Reply'
-    expect(page).to have_content 'Your reply message was successfully sent!'
-    count = receiver.mailbox.inbox.count
-    expect(count).to eq 1
+  describe 'user/receiver replies to message' do
+    before do
+      count = receiver.mailbox.inbox.count
+      expect(count).to eq 1
+      conversation = receiver.mailbox.inbox.first
+      receipts = conversation.receipts_for receiver
+      receiver.reply_to_sender(receipts[0], "Nullam interdum auctor ipsum")
+    end
+
+    it 'sender should have 1 new message in the inbox' do
+      count = sender.mailbox.inbox.count
+      expect(count).to eq 1
+    end
+
+    it 'user should see the replied message' do
+      conversation = sender.mailbox.inbox.first
+      expect(conversation.messages[2]) == 'Nullam interdum auctor ipsum'
+    end
   end
 
-#   describe 'checking sender has received the reply' do
-#     before do
-#       login_as(sender, scope: :user, name: 'sender', email: 'sender@mail.com')
-#       visit mailbox_inbox_path
-#       #   click_on 'View'
-#     end
-
-#     it 'sender should have 1 new message in the inbox' do
-#       count = sender.mailbox.inbox.count
-#       expect(count).to eq 1
-#     end
-#     it "sender receives user's message" do
-#       expect(page).to have_content 'Nullam interdum'
-#     end
-#   end
 end
